94e87f4ff6559bf70f4769655733893be9d965a3
RohanSirr <shubhamgupta642004@gmail.com>
2025-07-28 22:29:06 +0530

    Updated the comments

--- Arrays/sizeOperator.cpp
+++ Arrays/sizeOperator.cpp
@@ -5,7 +5,7 @@ int main(){
     int arr[]={1,2,3,4,5,6,7,8,9,0};
     int n= sizeof(arr)/4;                   // agr memory sizes yaad h to direct size se divide kr do 
 
-    int n= sizeof(arr)/sizeof(int);         // wrna simple sizeof jo b data type h usko likh k edivide kr do 
+    int n= sizeof(arr)/sizeof(int);         // wrna simple sizeof jo b data type h usko likh k divide kr do 
     cout<<n;
 
 }
