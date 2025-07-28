56475d3d86d8ff9897a331d5771153a59da3759b
RohanSirr <shubhamgupta642004@gmail.com>
2025-07-28 19:56:23 +0530

    practicing

--- Arrays/PrintArr.cpp
+++ Arrays/PrintArr.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter a integers:" << endl;
-    cin>>n;
-    int arr[n]; 
+// int main() {
+//     int n;
+//     cout << "Enter a integers:" << endl;
+//     cin>>n;
+//     int arr[n]; 
 
    
 
-    for (int i = 0; i < n; i++) {
-        cout << "Enter number " << i + 1 << ": ";
-        cin >> arr[i];
-    }
-
-    cout << "You entered:" << endl;
-
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
-
-
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int arr[5];
-//     for(int i=0; i<5; i++){
-//         cin>>arr[i];
+//     for (int i = 0; i < n; i++) {
+//         cout << "Enter number " << i + 1 << ": ";
+//         cin >> arr[i];
 //     }
-//     for (int i=0; i<5; i++){
-//         cout<<arr[i]<<" ";
+
+//     cout << "You entered:" << endl;
+
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
 //     }
+//     cout << endl;
+
+//     return 0;
 // }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    for(int i=0; i<5; i++){
+        cin>>arr[i];
+    }
+    for (int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+}
 
 
 
