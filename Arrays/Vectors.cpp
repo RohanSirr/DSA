230bbfd56dc5d41e273f6bc2cadab670e666acf6
RohanSirr <shubhamgupta642004@gmail.com>
2025-07-29 22:37:01 +0530

    Questions

---
+++ Arrays/Vectors.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n = 5;
+    vector<int> arr(n,7);
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(2);
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+
+
+
+
+
+}
