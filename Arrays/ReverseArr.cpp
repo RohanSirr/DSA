8542dc487fcec004694b2d6aacb2770e393449fc
RohanSirr <shubhamgupta642004@gmail.com>
2025-08-02 22:44:24 +0530

    Tried some another values

--- Arrays/ReverseArr.cpp
+++ Arrays/ReverseArr.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {10,20,30,40,50,60};
+    int arr[] = {10,20,30,40,50,60,70,50};
     int n = sizeof(arr)/4;
     for (int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
     cout<<endl;
-    int i = 0, j = n-1;
+    int i = 2, j = 5;
     while(i<j){
         swap(arr[i],arr[j]);
         i++;
