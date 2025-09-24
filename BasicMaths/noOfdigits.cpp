b5e55bf3aa11ff33eee9e8189392ef79b39382b8
RohanSirr <shubhamgupta642004@gmail.com>
2025-09-24 12:03:02 +0530

    added a new optimal method

--- BasicMaths/noOfdigits.cpp
+++ BasicMaths/noOfdigits.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main () {
     int n;
     cin>>n;
-    int cnt=0;
-    int lastDigit;
-    while(n>0){
-        lastDigit=n%10;
-        cnt++;
-        n=n/10;
-    }
+    int cnt = (int)(log10(n) + 1);
+    // int lastDigit;
+    // while(n>0){
+    //     lastDigit=n%10;             brute force method
+    //     cnt++;
+    //     n=n/10;
+    // }
     cout<<cnt;
 }
+
