b1115149c5bc45057a6b4659a18eb9b2f9dd9093
RohanSirr <shubhamgupta642004@gmail.com>
2025-09-29 20:26:55 +0530

    Character hashing

---
+++ Hashing/characterHash.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Enter a string: ";
+    cin>>s;
+
+    int hash[26] = {0};
+    for (int i=0; i<s.size(); i++){
+        hash[s[i]-'a']++;
+    }
+
+    int q;
+    cout<<"Enter number of queries: ";
+    cin>>q;
+    while(q--){
+        char c;
+        
+        cin>>c;
+
+        cout<<hash[c-'a']<<endl;
+    }
+    return 0;
+}
